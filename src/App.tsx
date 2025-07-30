4334258dcc8c99950ec40eac150487beb1597bd6
Eduardo Mendes <edumendes@gmail.com>
2025-07-30 15:31:50 -0400

    Logo component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./styles/global.css";
 
 import { Container } from "./components/Container";
 import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
 
 export function App() {
   console.log("Hello, Chronos Pomodoro!");
@@ -10,11 +11,11 @@ export function App() {
   return (
     <>
       <Container>
-        <Heading>Logo</Heading>
+        <Logo>Logo</Logo>
       </Container>
 
       <Container>
-        <section>MENU</section>
+        <Heading>MENU</Heading>
       </Container>
 
       <Container>
